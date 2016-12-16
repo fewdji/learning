00b394554e0ae803f83bf0589a17916fcaf7d212
Ilya Garkavenko <fewdji@gmail.com>
2016-12-17 00:26:43 +0300

    added Factory Method

---
+++ DesignPatterns/Creational/FactoryMethod.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace DesignPatterns\Creational\FactoryMethod;
+
+/**
+ * Фабричный метод определяет интерфейс для создания объекта, но позволяет подклассам решать, какой класс будет инстанцировать
+ */
+interface Factory
+{
+    public function createFurniture();
+}
+
+/**
+ * Фабрика стульев
+ */
+class ChairFactory implements Factory
+{
+    /**
+     * Создает объект Chair
+     * @return Chair
+     */
+    public function createFurniture(): Chair
+    {
+        return new Chair();
+    }
+}
+
+/**
+ * Фабрика столов
+ */
+class TableFactory implements Factory
+{
+    /**
+     * Создает объект Table
+     * @return Table
+     */
+    public function createFurniture(): Table
+    {
+        return new Table();
+    }
+}
+
+/**
+ * Абстрактный класс мебели
+ */
+abstract class Furniture
+{
+    private $color;
+    abstract public function getName();
+}
+
+class Chair extends Furniture
+{
+    /**
+     * Получить название мебели
+     * @return string
+     */
+    public function getName()
+    {
+        return 'It\'s the chair!';
+    }
+}
+
+class Table extends Furniture
+{
+    /**
+     * Получить название мебели
+     * @return string
+     */
+    public function getName()
+    {
+        return 'It\'s the table!';
+    }
+}
+
+/**
+ * Тесты
+ */
+$factories = array(new ChairFactory(), new TableFactory());
+
+foreach ($factories as $factory)
+{
+    print $factory->createFurniture()->getName() . PHP_EOL;
+}
